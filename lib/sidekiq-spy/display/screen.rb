9f0a77b16f98dad2e535696373f8a4ed28cc4a24
tiredpixel <tp@tiredpixel.com>
2013-10-15 00:07:34 +0100

    Construct fully-operational header panel.
    The header panel displays program, Redis, and Sidekiq stats metrics,
    using `Spy::Stats` and snazzy lambdas.
    
    The structure for a panel is:
    
    - an array of rows/lines
    - each containing an array of length 3 (yup)
    - each of the structure: [relative width, left-aligned, right-aligned]
    
    Dividers are used to ... wait for it ... divide (columns).
    
    This makes the program operational, with main stats monitored. Hurrah!

--- lib/sidekiq-spy/display/screen.rb
+++ lib/sidekiq-spy/display/screen.rb
@@ -5,6 +5,8 @@ module SidekiqSpy
   module Display
     class Screen
       
+      include Translatable
+      
       def initialize
         Curses.init_screen
         Curses.nl
@@ -15,6 +17,7 @@ module SidekiqSpy
         @width  = Curses.cols
         
         @panels = {
+          :header => panel_header,
         }
       end
       
@@ -26,6 +29,52 @@ module SidekiqSpy
       
       def refresh
         @panels.each { |pname, panel| panel.refresh }
+      end
+      
+      private
+      
+      def panel_header
+        stats = Spy::Stats.new
+        
+        structure = [
+          [
+            [1, t[:program], nil],
+            [1, nil,         -> { Time.now.strftime("%T %z") }],
+          ],
+          [
+            [2, t[:redis][:connection], -> { stats.connection }],
+            [1, t[:redis][:namespace],  -> { stats.namespace }],
+          ],
+          [
+            [1, t[:redis][:version],     -> { stats.redis_version }],
+            [1, t[:redis][:uptime],      -> { stats.uptime }],
+            [1, t[:redis][:connections], -> { stats.connections }],
+          ],
+          [
+            [1, t[:redis][:memory],      -> { stats.memory }],
+            [1, t[:redis][:memory_peak], -> { stats.memory_peak }],
+            [1, nil,                     nil],
+          ],
+          nil,
+          [
+            [1, t[:sidekiq][:busy],      -> { stats.busy }],
+            [1, t[:sidekiq][:retries],   -> { stats.retries }],
+            [1, t[:sidekiq][:processed], -> { stats.processed }],
+          ],
+          [
+            [1, t[:sidekiq][:enqueued],  -> { stats.enqueued }],
+            [1, t[:sidekiq][:scheduled], -> { stats.scheduled }],
+            [1, t[:sidekiq][:failed],    -> { stats.failed }],
+          ],
+          nil,
+        ]
+        
+        opts = {
+          :divider_l => t[:divider][:left],
+          :divider_r => t[:divider][:right],
+        }
+        
+        Display::Panel.new(structure.length, @width, 0, 0, structure, opts)
       end
       
     end
