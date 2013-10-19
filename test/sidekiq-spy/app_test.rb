f8e8d8748bee4a05aa863179261915f5a5e907ec
tiredpixel <tp@tiredpixel.com>
2013-10-19 19:05:44 +0100

    Move App.config tests to App tests from Config tests.
    This is probably a better home for them, and enables the Config tests'
    dependency on `lib/sidekiq-spy/app.rb` to be removed.

--- test/sidekiq-spy/app_test.rb
+++ test/sidekiq-spy/app_test.rb
@@ -13,6 +13,94 @@ describe SidekiqSpy::App do
     @app.running.must_equal false
   end
   
+  
+  describe "configure block main" do
+    before do
+      @app.configure do |c|
+        c.namespace = 'resque'
+        c.interval  = 1
+      end
+      
+      @config = @app.config
+    end
+    
+    it "configures namespace" do
+      @config.namespace.must_equal 'resque'
+    end
+    
+    it "configures interval" do
+      @config.interval.must_equal 1
+    end
+  end
+  
+  describe "configure block url" do
+    before do
+      @app.configure do |c|
+        c.url = 'redis://da.example.com:237/42'
+      end
+      
+      @config = @app.config
+    end
+    
+    it "configures host" do
+      @config.host.must_equal 'da.example.com'
+    end
+    
+    it "configures port" do
+      @config.port.must_equal 237
+    end
+    
+    it "configures database" do
+      @config.database.must_equal 42
+    end
+  end
+  
+  describe "configure block url-brief" do
+    before do
+      @app.configure do |c|
+        c.url = 'redis://da.example.com'
+      end
+      
+      @config = @app.config
+    end
+    
+    it "configures host" do
+      @config.host.must_equal 'da.example.com'
+    end
+    
+    it "configures port" do
+      @config.port.must_equal 6379
+    end
+    
+    it "configures database" do
+      @config.database.must_equal 0
+    end
+  end
+  
+  describe "configure block non-url" do
+    before do
+      @app.configure do |c|
+        c.host      = 'da.example.com'
+        c.port      = 237
+        c.database  = 42
+      end
+      
+      @config = @app.config
+    end
+    
+    it "configures host" do
+      @config.host.must_equal 'da.example.com'
+    end
+    
+    it "configures port" do
+      @config.port.must_equal 237
+    end
+    
+    it "configures database" do
+      @config.database.must_equal 42
+    end
+  end
+  
   describe "#start" do
     before do
       @app.configure do |c|
