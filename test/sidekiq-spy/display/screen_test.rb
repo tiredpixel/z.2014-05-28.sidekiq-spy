d997f7cd93cea36be7aa94ee58bd700c49416060
tiredpixel <tp@tiredpixel.com>
2013-10-20 16:29:14 +0100

    Add Display::Screen tests.
    Test main functionality of `Display::Screen`, such as it is. Nuke
    `Curses` as part of these tests, and replace it with a stub.
    
    These tests are rather brittle.

---
+++ test/sidekiq-spy/display/screen_test.rb
@@ -0,0 +1,87 @@
+require File.expand_path('../../../helper', __FILE__)
+
+require File.expand_path('../../../../lib/sidekiq-spy/display/screen', __FILE__)
+
+
+module SidekiqSpy
+  module Display
+    module Panels
+      class Header; end
+      class RedisStats; end
+      class SidekiqStats; end
+    end
+  end
+end
+
+
+describe SidekiqSpy::Display::Screen do
+  
+  before do
+    Object.send(:remove_const, :Curses) if Object.constants.include?(:Curses)
+    
+    Curses = stub(
+      :init_screen  => nil,
+      :close_screen => nil,
+      :nl           => nil,
+      :noecho       => nil,
+      :curs_set     => nil,
+      :lines        => 24,
+      :cols         => 80
+    )
+    
+    @panel = stub(
+      :close => nil
+    )
+    
+    SidekiqSpy::Display::Panels::Header.stubs(:new).returns(@panel)
+    SidekiqSpy::Display::Panels::RedisStats.stubs(:new).returns(@panel)
+    SidekiqSpy::Display::Panels::SidekiqStats.stubs(:new).returns(@panel)
+    
+    @screen = SidekiqSpy::Display::Screen.new
+  end
+  
+  describe "#initialize" do
+    it "inits screen" do
+      Curses.expects(:init_screen)
+      
+      SidekiqSpy::Display::Screen.new
+    end
+    
+    it "sets height" do
+      @screen.instance_variable_get(:@height).must_equal 24
+    end
+    
+    it "sets width" do
+      @screen.instance_variable_get(:@width).must_equal 80
+    end
+  end
+  
+  describe "#close" do
+    it "closes screen" do
+      Curses.expects(:close_screen)
+      
+      @screen.close
+    end
+    
+    it "closes panels" do
+      @panel2 = stub
+      @panel2.expects(:close)
+      
+      @screen.instance_variable_set(:@panels, { :panel2 => @panel2 })
+      
+      @screen.close
+    end
+  end
+  
+  describe "#refresh" do
+    it "refreshes panels" do
+      @panel2 = stub
+      @panel2.expects(:refresh)
+      
+      @screen.instance_variable_set(:@panels, { :panel2 => @panel2 })
+      
+      @screen.refresh
+    end
+  end
+  
+end
