c79894ef6f07ecce31ae1b8964ac5d345f6142ae
Prabhakar Chaganti <prabhakar@ylastic.com>
2016-09-25 18:45:07 -0400

    updated to load when using Sidekiq4. Changed to use the API changes in SK. There is still an issue when changing panels that needs to be tracked down.
    Signed-off-by: Prabhakar Chaganti <prabhakar@ylastic.com>

--- lib/sidekiq-spy/spy/workers.rb
+++ lib/sidekiq-spy/spy/workers.rb
@@ -5,31 +5,30 @@ require 'sidekiq'
 module SidekiqSpy
   module Spy
     class Workers
-      
+
       include Spy::Dataspyable
-      
+
       def initialize
         @workers = Sidekiq::Workers.new
-        
+
         refresh
       end
-      
+
       def refresh
         h = []
-        
-        @workers.each do |worker, msg|
+
+        @workers.each do |process_id, thread_id, work|
           h << {
-            :name       => worker,
-            :queue      => msg['queue'],
-            :class      => msg['payload']['class'],
-            :args       => msg['payload']['args'],
-            :started_at => (msg['run_at'].is_a?(Numeric) ? Time.at(msg['run_at']) : Time.parse(msg['run_at'])),
+              :name => process_id,
+              :queue => work['queue'],
+              :class => work['payload']['class'],
+              :args => work['payload']['args'],
+              :started_at => (work['run_at'].is_a?(Numeric) ? Time.at(work['run_at']) : Time.parse(work['run_at'])),
           }
         end
-        
         @data = h
       end
-      
+
     end
   end
 end
